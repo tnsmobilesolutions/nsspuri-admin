687eacf13b925618dfcd81059491e78c9278b2e6
Dhananjay Moharana <csdhananjaymoharana@gmail.com>
2024-01-20 13:00:44 +0530

    container background image added

--- lib/constant/pdf_delegatecard.dart
+++ lib/constant/pdf_delegatecard.dart
@@ -13,10 +13,13 @@ class DisplayPdf {
   static const PIXEL_PER_INCH = 72.0;
 
   static void delegatePDF(
-      List<DevoteeModel> deviteeselecteddata, BuildContext context) async {
+      List<DevoteeModel> devoteeselecteddata, BuildContext context) async {
     final img1 = await rootBundle.load('assets/images/blood.png');
     final imageBytes1 = img1.buffer.asUint8List();
     pw.Image bloodImage = pw.Image(pw.MemoryImage(imageBytes1));
+    final img4 = await rootBundle.load('assets/images/Group 12.png');
+    final imageBytes4 = img4.buffer.asUint8List();
+    pw.Image containerImage = pw.Image(pw.MemoryImage(imageBytes4));
     final img2 = await rootBundle.load('assets/images/nsslogo.png');
     final imageBytes2 = img2.buffer.asUint8List();
     pw.Image nssLogo = pw.Image(pw.MemoryImage(imageBytes2));
@@ -103,235 +106,15 @@ class DisplayPdf {
 
     pw.Widget buildCard(pw.Context context, DevoteeModel cardData) {
       return pw.Container(
-          height: 4.4 * PIXEL_PER_INCH,
-          width: 2.7 * PIXEL_PER_INCH,
-          margin: const pw.EdgeInsets.all(8),
-          padding: const pw.EdgeInsets.all(8),
-          decoration: pw.BoxDecoration(
-            border: pw.Border.all(color: PdfColors.black),
-          ),
-          child: pw.Padding(
-              padding: const pw.EdgeInsets.all(5.0),
-              child: pw.Column(children: [
-                pw.Column(children: [
-                  pw.SizedBox(
-                      height: 30,
-                      width: 1000,
-                      child: pw.Row(children: [
-                        pw.Expanded(
-                            child: pw.Row(children: [
-                          pw.Expanded(
-                            //  flex: 3,
-                            child: pw.Container(
-                              child: cardData.bloodGroup == "Don't know" ||
-                                      cardData.bloodGroup == null
-                                  ? pw.Container(
-                                      width: 75,
-                                      height: 60,
-                                      decoration: const pw.BoxDecoration(
-                                        shape: pw.BoxShape.rectangle,
-                                      ),
-                                    )
-                                  : pw.Center(
-                                      child: pw.Stack(
-                                        children: [
-                                          pw.Container(
-                                            width: 30,
-                                            height: 60,
-                                            decoration: const pw.BoxDecoration(
-                                              shape: pw.BoxShape.rectangle,
-                                            ),
-                                            child: bloodImage,
-                                          ),
-                                          pw.Positioned(
-                                            top: 7,
-                                            left: 0,
-                                            right: 0,
-                                            child: pw.SizedBox(
-                                              width: 30,
-                                              height: 20,
-                                              child: pw.Center(
-                                                child: pw.Text(
-                                                  "${cardData.bloodGroup}",
-                                                  style: pw.TextStyle(
-                                                    fontSize: 5,
-                                                    color: PdfColors.white,
-                                                    // color: PdfColors.fromARGB(
-                                                    //     255,
-                                                    //     255,
-                                                    //     255,
-                                                    //     255),
-                                                    fontWeight:
-                                                        pw.FontWeight.bold,
-                                                  ),
-                                                ),
-                                              ),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                            ),
-                          ),
-                        ]))
-                      ]))
-                ]),
-
-//
-
-//                                       if (cardData.status == "paid" ||
-//                                           cardData.status == "printed")
-//                                         pw.Positioned(
-//                                           top: 50,
-//                                           left: 105,
-//                                           child: pw.Transform.rotate(
-//                                             angle: 12,
-//                                             child: pw.Padding(
-//                                               padding:
-//                                                   const pw.EdgeInsets.all(4.0),
-//                                               child: pw.Container(
-//                                                 decoration: pw.BoxDecoration(
-//                                                     border: pw.Border.all(
-//                                                         // color:  Color
-//                                                         //     .fromARGB(
-//                                                         //     255,
-//                                                         //     44,
-//                                                         //     7,
-//                                                         //     209),
-//                                                         width: 4),
-//                                                     borderRadius:
-//                                                         pw.BorderRadius
-//                                                             .circular(4)),
-//                                                 child: pw.Padding(
-//                                                   padding:
-//                                                       const pw.EdgeInsets.all(
-//                                                           4.0),
-//                                                   child: pw.Text(
-//                                                     'PAID',
-//                                                     style: pw.TextStyle(
-//                                                       fontSize: 40.0,
-//                                                       fontWeight:
-//                                                           pw.FontWeight.bold,
-//                                                       // color: Color.fromARGB(
-//                                                       //     255,
-//                                                       //     44,
-//                                                       //     7,
-//                                                       //     209),
-//                                                     ),
-//                                                   ),
-//                                                 ),
-//                                               ),
-//                                             ),
-//                                           ),
-//                                         ),
-//                                     ],
-//                                   ),
-//                                 ),
-//                               ],
-//                             ),
-//                           ),
-//                           pw.SizedBox(
-//                             height: 8,
-//                           ),
-//                           cardData.name != null
-//                               ? pw.Text(
-//                                   "${cardData.name}",
-//                                   style: pw.TextStyle(
-//                                     color: PdfColors.deepOrange,
-//                                     fontSize: 20,
-//                                     fontWeight: pw.FontWeight.bold,
-//                                   ),
-//                                 )
-//                               : pw.Text(""),
-//                           pw.SizedBox(
-//                             height: 6,
-//                           ),
-//                           pw.Expanded(
-//                             child: pw.Row(
-//                               mainAxisAlignment:
-//                                   pw.MainAxisAlignment.spaceEvenly,
-//                               children: [
-//                                 pw.Flexible(
-//                                   // flex: 2,
-//                                   child: pw.Column(
-//                                     crossAxisAlignment:
-//                                         pw.CrossAxisAlignment.start,
-//                                     children: [
-//                                       pw.Expanded(
-//                                           //  flex: 2,
-//                                           child: cardData.sangha != null
-//                                               ? pw.Text(
-//                                                   cardData.sangha ?? '',
-//                                                   style: pw.TextStyle(
-//                                                     color: PdfColors.black,
-//                                                     fontSize: 14,
-//                                                     fontWeight:
-//                                                         pw.FontWeight.bold,
-//                                                   ),
-//                                                 )
-//                                               : pw.SizedBox()),
-//                                       pw.Expanded(
-//                                         //flex: 2,
-//                                         child: cardData.devoteeCode != null
-//                                             ? pw.Text(
-//                                                 _toPascalCase(cardData
-//                                                     .devoteeCode
-//                                                     .toString()),
-//                                                 style: pw.TextStyle(
-//                                                   color: PdfColors.deepOrange,
-//                                                   fontSize: 15,
-//                                                   fontWeight:
-//                                                       pw.FontWeight.bold,
-//                                                 ),
-//                                               )
-//                                             : pw.Text(""),
-//                                       ),
-//                                       pw.Expanded(
-//                                         // flex: 2,
-//                                         child: pw.Text(
-//                                           "Sri Sri Thakura Charanasrita",
-//                                           style: pw.TextStyle(
-//                                               fontSize: 12,
-//                                               fontWeight: pw.FontWeight.bold),
-//                                         ),
-//                                       ),
-//                                       pw.Expanded(
-//                                         //flex: 1,
-//                                         child: pw.Text(
-//                                           "",
-//                                           style: pw.TextStyle(
-//                                               fontSize: 10,
-//                                               fontWeight: pw.FontWeight.bold),
-//                                         ),
-//                                       ),
-//                                       pw.Expanded(
-//                                         //flex: 1,
-//                                         child: pw.Text(
-//                                           "",
-//                                           style: pw.TextStyle(
-//                                               fontSize: 10,
-//                                               fontWeight: pw.FontWeight.bold),
-//                                         ),
-//                                       ),
-//                                       pw.Expanded(
-//                                         //flex: 1,
-//                                         child: pw.Text(
-//                                           'Secretary',
-//                                           style: pw.TextStyle(
-//                                               fontSize: 14,
-//                                               fontWeight: pw.FontWeight.bold),
-//                                         ),
-//                                       ),
-//                                     ],
-//                                   ),
-//                                 ),
-//
-//                               ],
-//                             ),
-//                           ),
-//                         ],
-//                       )),
-              ])));
+        height: 4.4 * PIXEL_PER_INCH,
+        width: 2.7 * PIXEL_PER_INCH,
+        margin: const pw.EdgeInsets.all(8),
+        padding: const pw.EdgeInsets.all(8),
+        decoration: pw.BoxDecoration(
+          border: pw.Border.all(color: PdfColors.black),
+        ),
+        child: containerImage,
+      );
     }
 
     final doc = pw.Document();
@@ -349,8 +132,8 @@ class DisplayPdf {
                   child: pw.GridView(
                     crossAxisCount: 2,
                     children: [
-                      for (int i = 0; i < deviteeselecteddata.length; i++)
-                        buildCard(context, deviteeselecteddata[i]),
+                      for (int i = 0; i < devoteeselecteddata.length; i++)
+                        buildCard(context, devoteeselecteddata[i]),
                     ],
                   )));
         },
