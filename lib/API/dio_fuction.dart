626f1efe33e333bbae92f9d1172977071d3d44bc
tns <amrutanshup@gmail.com>
2024-04-04 01:23:32 +0530

    firebase

--- lib/API/dio_fuction.dart
+++ lib/API/dio_fuction.dart
@@ -2,8 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:sdp/firebase/firebase_remote_config.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-String baseUrl = RemoteConfigHelper().getapiBaseURL;
-// String baseUrl = "https://staging-api.nsspuri.org/";
+// String baseUrl = RemoteConfigHelper().getapiBaseURL;
+String baseUrl = "api.nsspuri.org/";
 // String baseUrl = "http://localhost:4400/";
 // String baseUrl = "http://127.0.0.1:4400/";
 
